e60c26f32429fa24a688e13ef0fd05e2ebdb5b87
VCG\victo <vcinca@labelgrup.com>
2022-11-04 15:16:35 +0100

    04/11/2022 - label_olympia - Creación de módulo de herramientas Olympia

---
+++ modules/label_olympia/label_olympia.php
@@ -0,0 +1,108 @@
+<?php
+/**
+* 2007-2022 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author    PrestaShop SA <contact@prestashop.com>
+*  @copyright 2007-2022 PrestaShop SA
+*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class Label_olympia extends Module
+{
+    protected $config_form = false;
+
+    public function __construct()
+    {
+        $this->name = 'label_olympia';
+        $this->tab = 'front_office_features';
+        $this->version = '1.0.0';
+        $this->author = 'Labelgrup Networks S.L.';
+        $this->need_instance = 1;
+
+        /**
+         * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
+         */
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Olympia tools');
+        $this->description = $this->l('Olympia web tool module');
+
+        $this->confirmUninstall = $this->l('Are you sure you want to unistall Olympia tools module?');
+
+        $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
+    }
+
+    /**
+     * Don't forget to create update methods if needed:
+     * http://doc.prestashop.com/display/PS16/Enabling+the+Auto-Update
+     */
+    public function install()
+    {
+        return parent::install() &&
+            $this->registerHook('header') &&
+            $this->registerHook('backOfficeHeader') &&
+            $this->registerHook('displayHome');
+    }
+
+    public function uninstall()
+    {
+        return parent::uninstall();
+    }
+
+    /**
+    * Add the CSS & JavaScript files you want to be loaded in the BO.
+    */
+    public function hookBackOfficeHeader()
+    {
+        if (Tools::getValue('module_name') == $this->name) {
+            $this->context->controller->addJS($this->_path.'views/js/back.js');
+            $this->context->controller->addCSS($this->_path.'views/css/back.css');
+        }
+    }
+
+    /**
+     * Add the CSS & JavaScript files you want to be added on the FO.
+     */
+    public function hookHeader()
+    {
+        $this->context->controller->addJS($this->_path.'/views/js/front.js');
+        $this->context->controller->addCSS($this->_path.'/views/css/front.css');
+    }
+
+    public function hookDisplayHome()
+    {
+        $sqlFilter = '
+            AND c.level_depth = 2 
+        ';
+        $limit = 'LIMIT 9';
+        $categories = Category::getCategories(Context::getContext()->language->id, true, true, $sqlFilter, '', $limit);
+        $this->smarty->assign([
+            'categories' => $categories[2]
+        ]);
+  
+        return $this->fetch('module:label_olympia/views/templates/hook/categories.tpl');
+    }
+}
---
+++ modules/label_olympia/views/templates/hook/categories.tpl
@@ -0,0 +1,36 @@
+<div class="elementor-section elementor-element elementor-element-4yej532 elementor-top-section elementor-section-full_width elementor-section-height-default elementor-section-height-default elementor-section-content-middle" data-element_type="section">
+    <div class="elementor-container elementor-column-gap-default">
+        <div class="elementor-row">
+            {foreach $categories as $category}
+                <div class="elementor-column elementor-element elementor-element-ht0p3k3 elementor-col-11 elementor-top-column" 
+                    data-element_type="column">
+                    <div class="elementor-column-wrap elementor-element-populated">
+                        <div class="elementor-widget-wrap">
+                            <div 
+                                class="elementor-widget elementor-element elementor-element-c54elor 
+                                    elementor-widget-icon-box elementor-view-default elementor-position-top 
+                                    elementor-vertical-align-top animated fadeInLeft" 
+                                data-animation="fadeInLeft" data-element_type="icon-box">
+                                <div class="elementor-widget-container">
+                                    <div class="elementor-icon-box-wrapper">
+                                        <div class="elementor-icon-box-icon">
+                                            <span class="elementor-icon elementor-animation-">
+                                                <i class="fa fa-balance-scale"></i>
+                                            </span>
+                                        </div>
+                                        <div class="elementor-icon-box-content">
+                                            <h3 class="elementor-icon-box-title">
+                                                <span>{$category.infos.name}</span>
+                                            </h3>
+                                            <div class="elementor-icon-box-description"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            {/foreach}
+		</div>
+    </div>
+</div>
